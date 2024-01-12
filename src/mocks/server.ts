5105c35c0b0ab474514bb8d4bbf8b5c0110a16be
jeeweb <52396673+jeeweb@users.noreply.github.com>
2024-01-12 19:48:53 +0900

    feat: msw 서버사이드 셋업 변경 - express

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,4 +1,16 @@
-import { setupServer } from "msw/node";
+import { createMiddleware } from "@mswjs/http-middleware";
+import express from "express";
 import { handlers } from "./handlers";
 
-export const server = setupServer(...handlers);
+const app = express();
+const port = 9090;
+
+app.use(express.json());
+app.use(createMiddleware(...handlers));
+
+app.listen(port, () => console.log(`Mock server is running on port: ${port}`));
+
+// import { setupServer } from "msw/node";
+// import { handlers } from "./handlers";
+
+// export const server = setupServer(...handlers);
